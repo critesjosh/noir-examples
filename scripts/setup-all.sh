fb6532a396f4fa72db87ac754a7234c764050ee9
Josh Crites <jc@joshcrites.com>
2025-06-13 19:56:13 -0400

    fix

--- scripts/setup-all.sh
+++ scripts/setup-all.sh
@@ -9,9 +9,8 @@ noirup
 
 echo "Installing latest Barretenberg..."
 curl -sSL https://raw.githubusercontent.com/AztecProtocol/aztec-packages/refs/heads/master/barretenberg/bbup/install | bash
-export PATH="$HOME/.bbup/bin:$PATH"
+export PATH="$HOME/.bb/:$PATH"
 echo "home: $HOME, path: $PATH"
-echo "export PATH=$HOME/.bbup/bin:$PATH" >> ~/.bashrc
 bbup
 
 echo "\n--- Compiling all Noir circuits ---"
